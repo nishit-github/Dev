c1d51e1190266e997ab4a259d26e2f22b37c5ac3
nishit-github <55799096+nishit-github@users.noreply.github.com>
2020-01-19 17:47:44 +0530

    Add files via upload

---
+++ mountazurefiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo -s
+
+yum update -y
+
+yum install cifs-utils -y
+
+mkdir "$4"
+ 
+mount -t cifs //"$1".file.core.windows.net/"$3" "$4" -o vers=3.0,username="$1",password="$2",dir_mode=0755,file_mode=0664
+ 
+linkpoint=$(echo "$4" | sed 's/.*\///')
+ 
+eval ln -s "$4" ~"$5"/"$linkpoint"
+
+echo "hello from $HOSTNAME" > "$4"/"$HOSTNAME".txt      
+
